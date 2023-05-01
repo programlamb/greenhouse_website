644d6d79d44bd8517b46fbec4df8cd0fcf602e77
Danil <lemb06@mail.ru>
2023-05-01 21:57:13 +0700

    Начало положено

--- greenhouse.py
+++ greenhouse.py
@@ -1,2 +1,20 @@
-import flask
+import datetime
+from flask import (
+    Flask,
+    render_template
+)
 
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandex_lyceum_secret_key'
+app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(days=365)
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html", title="Site")
+
+
+if __name__ == "__main__":
+    app.run(host="localhost")
